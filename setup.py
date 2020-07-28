26a11701ffdb148f23ffa7f2fc2c2df4dee3c106
aviadlevis <aviad.levis@gmail.com>
2020-07-27 17:46:01 -0700

    added inoisy submodule

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from numpy.distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
-import os
+import subprocess
 
 
 extensions = [
@@ -15,14 +15,6 @@ extensions = [
         depends=['src/noisy/noisy.h', 'src/noisy/evolve.c', 'src/noisy/image.c',
                  'src/noisy/model_disk.c', 'src/noisy/model_uniform.c']
     ),
-    Extension(
-        'hgrf_core',
-        sources=['src/hgrf/hgrf.pyx', 'src/hgrf/hdf5_utils.c'],
-        include_dirs=[numpy.get_include(), '/home/aviad/Code/hypre/src/hypre/include/',
-                      '/usr/lib/x86_64-linux-gnu/openmpi/include/', '/home/aviad/anaconda3/envs/eht/include/'],
-        libraries=['gsl', 'blas', 'mpi', 'hdf5', 'HYPRE'],
-        library_dirs=['/usr/lib/x86_64-linux-gnu/openmpi/lib/', '/home/aviad/Code/hypre/src/hypre/lib/'],
-    )
 ]
 
 setup(
@@ -39,3 +31,5 @@ setup(
                       "future"],
     ext_modules=cythonize(extensions)
 )
+
+subprocess.Popen(["make general_xy"], shell=True, stdout=subprocess.PIPE, cwd="./inoisy")
